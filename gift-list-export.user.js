835d8fc5eb4651318d3a162801c8a93808aa7516
Dave Martinez <git@mrdave.dev>
2024-12-10 21:42:49 -0800

    Fix script for new Amazon UI

--- gift-list-export.user.js
+++ gift-list-export.user.js
@@ -19,6 +19,7 @@
   exportButton.innerHTML = '<span class="a-button-inner"><span class="a-button-text">Export gift details</span></span>'
 
   exportButton.addEventListener('click', async () => {
+    
     // Requests need a CSRF token; we find it on the page and store it
     const tokenQuery = Array.from(document.querySelectorAll('script[data-a-state]'))
       .map(result => {
@@ -30,6 +31,7 @@
       })
       .filter(result => !!result)
 
+
     // If we can't find the token, we won't be able to get the addresses
     if (tokenQuery.length !== 1) {
       throw new Error('Cannot find CSRF token; unable to export details')
@@ -38,20 +40,20 @@
     const token = tokenQuery[0]
 
     // Each item row has a `data-a-modal` attribute with data for the modal
-    const itemQueryResults = Array.from(document.querySelectorAll('[data-a-modal]'))
-      // The data-a-modal attribute contains a JSON string of data about the gift
+    const itemQueryResults = Array.from(document.querySelectorAll(`script[type=a-state][data-a-state='{"key":"tylDetailPageState"}']`))
       .map(result => {
         try {
-          return JSON.parse(result.getAttribute('data-a-modal'))
+          const modalData = JSON.parse(result.innerText)
+          return modalData
         } catch (e) {
           return null
         }
       })
-      // However, there exist some elements in the results that are not item rows
-      // So we check for presence the `gift` attribute in the parsed data
-      .filter(result => !!result && !!result.gift)
-      // Map each item in to a request for the item's details
+      .filter(result => {
+        return !!result && !!result.gift
+      })
       .map(({ gift, registryId, index, isPrimaryRegistrant }) => new Promise((resolve, reject) => {
+
         const giftRequest = {
           gift,
           registryId,
@@ -59,49 +61,50 @@
           primaryRegistrant: isPrimaryRegistrant
         }
 
-        const params = new URLSearchParams()
-        params.set('giftRequest', JSON.stringify(giftRequest))
-        params.set('token', token)
 
-        const formData = new FormData()
-        formData.append('giftRequest', JSON.stringify(giftRequest))
-        formData.append('token', token)
-
-        const request = new XMLHttpRequest()
+        const params = new URLSearchParams();
+        params.set('registryId', '2MSXVAPN0SUAA');
+        params.set('giftIndex', '0');
+        params.set('isPrimaryRegistrant', 'true');
+        params.set('primaryOwnerCustomerId', 'A45579H563N1Z');
+        params.set('gift', JSON.stringify(giftRequest.gift));
+        params.set('registryShippingAddressId', 'A3R5EH9ZY1D1OZ');
+        
+        const request = new XMLHttpRequest();
+        
+        // Add event listener for response handling
         request.addEventListener('load', (_event) => {
-          // The request returns HTML, so we add it to a virtual element
-          const element = document.createElement('div')
-          element.innerHTML = request.response
-          // The address is stored in the second ([1]) result
-
-          // START HERE -- Remove the words 'purchase address' from the element
-          // instead of trying to parse the address
-          const addressElement = Array.from(element.querySelectorAll('.br-tyl-details-wrapper .a-section'))[1]
-          const cleanAddress = addressElement.innerText.split(/\n?\s\s+?/g).filter(y => y.length > 0).join(' ')
-
-          if (!cleanAddress) {
-            // This is okay; some people choose not to include their address
-            console.warn('Could not find address elements', gift)
-          }
-
-          const row = [gift.giftGiverName, gift.displayableGiftDate, gift.productTitle, cleanAddress]
-            // Wrap the data in quotes for the CSV export, since product titles and addresses
-            // may contain a comma
-            .map(data => `"${data}"`)
-
-          return resolve(row)
-        })
-
-        request.open('POST', 'https://www.amazon.com/baby-reg/thankyoulist/detail?sif_profile=BabyRegistryThankYouList_NA')
-        request.setRequestHeader('accept', 'text/html,*/*')
-        request.setRequestHeader('content-type', 'application/x-www-form-urlencoded')
-        request.setRequestHeader('downlink', '10')
-        request.setRequestHeader('rtt', '0')
-        request.setRequestHeader('x-requested-with', 'XMLHttpRequest')
-        request.send(params.toString())
+          
+          const element = document.createElement('div');
+          element.innerHTML = request.response;
+        
+          const addressElement = Array.from(element.querySelectorAll('#br-tyl-details-wrapper.a-section'))[0];
+          const cleanAddress = element.querySelector('#br-tyl-gifter-address-0')
+            .innerText
+            .split(/\n?\s\s+?/g)
+            .filter((y) => y.length > 0)
+            .join(' ');
+        
+          const row = [gift.giftGiverName, gift.displayableGiftDate, gift.productTitle, cleanAddress];
+          resolve(row);
+        });
+        
+        // Open the request
+        request.open('POST', 'https://www.amazon.com/baby-reg/thankyoulist/details');
+        
+        // Set headers (must be after `open`)
+        request.withCredentials = true
+        request.setRequestHeader('Referrer', 'https://www.amazon.com/baby-reg/thankyoulist/2MSXVAPN0SUAA?ref_=br_dsk_tbnr_ty');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.setRequestHeader('Accept', 'text/html,*/*');
+        request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        
+        // Send the request with properly encoded parameters
+        request.send(params.toString());        
       }))
 
     const allResults = await Promise.all(itemQueryResults)
+
     const headerRows = ['Gift giver name', 'Date', 'Product', 'Address']
     const csvContent = `${headerRows.join(',')}\n` +
                 allResults.map(rowDataList => rowDataList.join(',')).join('\n')
@@ -112,9 +115,9 @@
 
     link.setAttribute('href', csvUrl)
     link.setAttribute('download', 'gift_export.csv')
-    document.body.appendChild(link) // Required for FF
+    document.body.appendChild(link)
     link.click()
   })
 
-  document.querySelector('.baby-reg-tyl-heading div div.a-span2').appendChild(exportButton)
+  document.querySelector('#br-tyl-heading > div > div.a-column.a-span10.a-text-right.a-spacing-none.a-span-last').appendChild(exportButton)
 })()
