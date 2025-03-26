6496c32aeafc6a9728d2d89ea7512fdd04338e2d
Kedric Daly <49046440+kedricdaly@users.noreply.github.com>
2025-03-26 15:16:58 -0400

    API fixes, Ease of Use and README updates (#34)
    * fix:add newly required 'token' POST request parameter
    
    * fix:update address element selector
    
    * fix:update API response gift date parameter after API change
    
    * feat:handle no address provided by gifter
    
    * feat:add registry constants block for ease of use
    
    Added javascript variables near beginning of script with a note to
    update them before running the script to help with ease of use.
    
    Each individual registry will have different parameters and the user
    will need to update these variables so that the script will run on their
    registry.
    
    * feat:improve csv output
    
    Multiple data columns returned from the Amazon gift registry use a comma
    as an internal separator. This commit wraps each column in quotes so
    that the internal comma separators do not break text-to-column in Excel.
    
    Additionally, because the double quote character, ", is used to wrap
    each output column, all double quote characters within a column are
    escaped with an additional double quote character to better adhere to
    csv standards.
    
    The end result should be a csv file that opens seemlessly in Excel
    with each row having the same output columns.
    
    * feat:parameterize output column separator
    
    Adds a const at the beginning of the script to allow for only one change
    being required to change the output column delimiter. E.g. if someone
    wants to change the output delimiter to a pipe, |, they can do so with
    only one change instead of two.
    
    * Update README.md
    
    * Adds an explanation of how to find the relevant POST parameters in the network tab.
    * Adds a screenshot of where the "Export gift details" button is with the current registry UI

--- gift-list-export.user.js
+++ gift-list-export.user.js
@@ -13,6 +13,12 @@
 (function () {
   'use strict'
 
+  // Update these based on your registry before running the script
+  const REGISTRY_ID = '2MSXVAPN0SUAA';
+  const PRIMARY_OWNER_CUSTOMER_ID = 'A45579H563N1Z';
+  const REGISTRY_SHIPPING_ADDRESS_ID = 'A3R5EH9ZY1D1OZ';
+  const OUTPUT_DELIMITER = ',';
+
   // Adds export button
   const exportButton = document.createElement('button')
   exportButton.className = 'a-button a-button-base'
@@ -63,12 +69,13 @@
 
 
         const params = new URLSearchParams();
-        params.set('registryId', '2MSXVAPN0SUAA');
+        params.set('registryId', REGISTRY_ID);
         params.set('giftIndex', '0');
         params.set('isPrimaryRegistrant', 'true');
-        params.set('primaryOwnerCustomerId', 'A45579H563N1Z');
+        params.set('primaryOwnerCustomerId', PRIMARY_OWNER_CUSTOMER_ID);
         params.set('gift', JSON.stringify(giftRequest.gift));
-        params.set('registryShippingAddressId', 'A3R5EH9ZY1D1OZ');
+        params.set('registryShippingAddressId', REGISTRY_SHIPPING_ADDRESS_ID);
+        params.set('token', token);
         
         const request = new XMLHttpRequest();
         
@@ -77,15 +84,16 @@
           
           const element = document.createElement('div');
           element.innerHTML = request.response;
-        
-          const addressElement = Array.from(element.querySelectorAll('#br-tyl-details-wrapper.a-section'))[0];
-          const cleanAddress = element.querySelector('#br-tyl-gifter-address-0')
+
+          const addressElement = Array.from(element.querySelectorAll('.br-tyl-item-details-address.a-section'))[0];
+          // There is an error caused if no address is given by one of the gifters
+          const cleanAddress = element.innerText.includes("No Address Given") ? "No Address Given" : element.querySelector('#br-tyl-gifter-address-0')
             .innerText
             .split(/\n?\s\s+?/g)
             .filter((y) => y.length > 0)
             .join(' ');
         
-          const row = [gift.giftGiverName, gift.displayableGiftDate, gift.productTitle, cleanAddress];
+          const row = [gift.giftGiverName, gift.giftDateString, gift.productTitle, cleanAddress];
           resolve(row);
         });
         
@@ -94,7 +102,7 @@
         
         // Set headers (must be after `open`)
         request.withCredentials = true
-        request.setRequestHeader('Referrer', 'https://www.amazon.com/baby-reg/thankyoulist/2MSXVAPN0SUAA?ref_=br_dsk_tbnr_ty');
+        request.setRequestHeader('Referrer', 'https://www.amazon.com/baby-reg/thankyoulist/'+REGISTRY_ID+'?ref_=br_dsk_tbnr_ty');
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         request.setRequestHeader('Accept', 'text/html,*/*');
         request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
@@ -105,9 +113,17 @@
 
     const allResults = await Promise.all(itemQueryResults)
 
+    // for csv output, we need to enclose all columns in quotes due to multiple
+    // columns containing commas, which is our csv separator.
+    // additionally, we need to replace all interior single quotes to comply
+    // with csv standard and so that it does not break when opening in excel.
+    // this happens often because many items the double quote character, "
+    // to represent inches e.g. 12" = 1 ft
+    const wrappedResults = allResults.map(rowDataList => rowDataList.map(d => `"${d.replaceAll('"','""')}"`));
+
     const headerRows = ['Gift giver name', 'Date', 'Product', 'Address']
-    const csvContent = `${headerRows.join(',')}\n` +
-                allResults.map(rowDataList => rowDataList.join(',')).join('\n')
+    const csvContent = `${headerRows.join(OUTPUT_DELIMITER)}\n` +
+                wrappedResults.map(rowDataList => rowDataList.join(OUTPUT_DELIMITER)).join('\n')
 
     const link = document.createElement('a')
     const csvData = new Blob([csvContent], { type: 'text/csv' })
